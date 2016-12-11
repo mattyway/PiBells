2f3e633a6df2d1a0ebe4fcca8a5939fcc124ca66
Matt Way <mattyway@gmail.com>
2016-12-12 00:20:25 +0800

    Added method to play music strings

--- src/Music.kt
+++ src/Music.kt
@@ -1,8 +1,10 @@
 import org.jfugue.integration.MusicXmlParser
+import org.jfugue.parser.ParserListener
 import org.jfugue.parser.ParserListenerAdapter
 import org.jfugue.player.Player
 import org.jfugue.temporal.TemporalPLP
 import org.jfugue.theory.Note
+import org.staccato.StaccatoParser
 import org.staccato.StaccatoParserListener
 import rx.Emitter
 import rx.Observable
@@ -11,14 +13,16 @@ import java.io.OutputStream
 import java.io.PrintStream
 
 class Music() {
-    val musicXmlParser = MusicXmlParser()
-    val temporalParser = TemporalPLP()
-    val staccatoParser = StaccatoParserListener()
+    class PassthroughListener : ParserListenerAdapter() {
+        var parserListener: ParserListener? = null
 
-    init {
-        musicXmlParser.addParserListener(staccatoParser)
-        musicXmlParser.addParserListener(temporalParser)
+        override fun onNoteParsed(note: Note?) {
+            parserListener?.onNoteParsed(note)
+        }
     }
+
+    val player = Player()
+    val passthroughParser = PassthroughListener()
 
     fun getNotes(): Observable<Note> {
         return Observable.fromEmitter<Note>({ emitter ->
@@ -29,14 +33,22 @@ class Music() {
             }
 
             emitter.setCancellation {
-                temporalParser.removeParserListener(listener)
+                passthroughParser.parserListener = listener
             }
-            temporalParser.addParserListener(listener)
+            passthroughParser.parserListener = listener
 
         }, Emitter.BackpressureMode.BUFFER)
     }
 
     fun play(musicXml: File) {
+        val musicXmlParser = MusicXmlParser()
+        val temporalParser = TemporalPLP()
+        val staccatoParser = StaccatoParserListener()
+
+        musicXmlParser.addParserListener(staccatoParser)
+        musicXmlParser.addParserListener(temporalParser)
+        temporalParser.addParserListener(passthroughParser)
+
         val originalStream = System.out
 
         val dummyStream = PrintStream(object : OutputStream() {
@@ -52,9 +64,21 @@ class Music() {
         // Restore original System.out so that we can output to the console later
         System.setOut(originalStream)
 
-        val player = Player()
         val musicXMLPattern = staccatoParser.pattern
         player.delayPlay(0, musicXMLPattern)
+        temporalParser.parse()
+    }
+
+    fun play(musicString: String) {
+        val stacattoParser = StaccatoParser()
+        val temporalParser = TemporalPLP()
+
+        stacattoParser.addParserListener(temporalParser)
+        temporalParser.addParserListener(passthroughParser)
+
+        stacattoParser.parse(musicString)
+
+        player.delayPlay(0, musicString)
         temporalParser.parse()
     }
 }
