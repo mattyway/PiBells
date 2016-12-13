31690ca6fb072d386e0e7d55c4f32a9745ed6ae3
Matt Way <mattyway@gmail.com>
2016-12-13 08:01:21 +0800

    Changed Music to use ManagedPlayer

--- src/Music.kt
+++ src/Music.kt
@@ -1,16 +1,17 @@
 import org.jfugue.integration.MusicXmlParser
+import org.jfugue.midi.MidiParserListener
 import org.jfugue.parser.ParserListener
 import org.jfugue.parser.ParserListenerAdapter
-import org.jfugue.player.Player
+import org.jfugue.player.ManagedPlayer
 import org.jfugue.temporal.TemporalPLP
 import org.jfugue.theory.Note
 import org.staccato.StaccatoParser
-import org.staccato.StaccatoParserListener
 import rx.Emitter
 import rx.Observable
 import java.io.File
 import java.io.OutputStream
 import java.io.PrintStream
+import kotlin.concurrent.thread
 
 class Music() {
     class PassthroughListener : ParserListenerAdapter() {
@@ -21,7 +22,7 @@ class Music() {
         }
     }
 
-    val player = Player()
+    val player = ManagedPlayer()
     val passthroughParser = PassthroughListener()
 
     fun getNotes(): Observable<Note> {
@@ -42,10 +43,10 @@ class Music() {
 
     fun play(musicXml: File) {
         val musicXmlParser = MusicXmlParser()
+        val midiParserListener = MidiParserListener()
         val temporalParser = TemporalPLP()
-        val staccatoParser = StaccatoParserListener()
 
-        musicXmlParser.addParserListener(staccatoParser)
+        musicXmlParser.addParserListener(midiParserListener)
         musicXmlParser.addParserListener(temporalParser)
         temporalParser.addParserListener(passthroughParser)
 
@@ -64,21 +65,26 @@ class Music() {
         // Restore original System.out so that we can output to the console later
         System.setOut(originalStream)
 
-        val musicXMLPattern = staccatoParser.pattern
-        player.delayPlay(0, musicXMLPattern)
+        thread {
+            player.start(midiParserListener.sequence)
+        }
         temporalParser.parse()
     }
 
     fun play(musicString: String) {
         val stacattoParser = StaccatoParser()
+        val midiParserListener = MidiParserListener()
         val temporalParser = TemporalPLP()
 
+        stacattoParser.addParserListener(midiParserListener)
         stacattoParser.addParserListener(temporalParser)
         temporalParser.addParserListener(passthroughParser)
 
         stacattoParser.parse(musicString)
 
-        player.delayPlay(0, musicString)
+        thread {
+            player.start(midiParserListener.sequence)
+        }
         temporalParser.parse()
     }
 }
